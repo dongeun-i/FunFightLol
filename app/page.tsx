1c460567c897b109ac265d23344690395266ee81
dongeun-i <dongeun1014@naver.com>
2025-11-30 21:12:56 +0900

    main page ui pulish

--- app/page.tsx
+++ app/page.tsx
@@ -3,62 +3,45 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col py-32 px-16 bg-white dark:bg-black ">
+        <div className="flex flex-col mb-10 items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            FunFight LoL
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="max-w-md text-lg leading-6 text-zinc-600 dark:text-zinc-400">
+            친구들과 함께 리그 오브 레전드를 더욱 재미있게 즐길 수 있도록 만들어진 보조 도구입니다.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex gap-4 text-base font-medium sm:flex-row mb-10">
+          <input type="text" className="w-full border border-zinc-300 rounded-md p-2" placeholder="소환사명을 입력하세요" />
+          <button className="shrink-0 grow-0 bg-zinc-900 text-white rounded-md p-2 hover:bg-zinc-800 cursor-pointer">찾기</button>
         </div>
+        {/* summer list */}
+        <div className="flex flex-row gap-4 mb-10">
+          <ul className="flex flex-row h-full w-full items-center justify-between">
+            <li className="flex flex-col gap-2 items-center justify-center">
+              <div className="w-[100px] h-[100px] bg-zinc-300 rounded-full"></div>
+              <span>Aatrox1</span>
+            </li>
+            <li className="flex flex-col gap-2 items-center justify-center">
+              <div className="w-[100px] h-[100px] bg-zinc-300 rounded-full"></div>
+              <span>Aatrox2</span>
+            </li>
+            <li className="flex flex-col gap-2 items-center justify-center">
+              <div className="w-[100px] h-[100px] bg-zinc-300 rounded-full"></div>
+              <span>Aatrox3</span>
+            </li>
+            <li className="flex flex-col gap-2 items-center justify-center">
+              <div className="w-[100px] h-[100px] bg-zinc-300 rounded-full"></div>
+              <span>Aatrox4</span>
+            </li>
+            <li className="flex flex-col gap-2 items-center justify-center">
+              <div className="w-[100px] h-[100px] bg-zinc-300 rounded-full"></div>
+              <span>Aatrox5</span>
+            </li>
+          </ul>
+        </div>
+        <button className="shrink-0 grow-0 bg-zinc-900 text-white rounded-md p-2 hover:bg-zinc-800 cursor-pointer">시작하기</button>
       </main>
     </div>
   );
